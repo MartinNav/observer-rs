7ec500084eb6dd266e9fb5da9217210be65dd030
MartinNav <martinnavratilu@gmail.com>
2024-07-31 14:28:56 +0200

    added the new function

--- src/action.rs
+++ src/action.rs
@@ -1,4 +1,20 @@
 
+ pub struct ActionObserver<T:std::clone::Clone>{
+    pub value:T,
+    previous:T,
+    func: fn(&T,&mut T)->(), 
+}
+impl<T:std::clone::Clone> ActionObserver<T> {
+     fn new(value:T,handle_function:fn(&T,&mut T)->())->Self {
+         ActionObserver{
+             value:value.clone(),
+             previous:value,
+             func:handle_function,
+         }
+     }
+
+    
+}
 
 #[cfg(test)]
 mod tests {
