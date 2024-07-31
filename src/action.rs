8c69f4fca2b7bb3ada6d1b86cdef1b233ded42a3
MartinNav <martinnavratilu@gmail.com>
2024-07-31 22:28:02 +0200

    fixed code in docs

--- src/action.rs
+++ src/action.rs
@@ -9,6 +9,7 @@ impl<'a, T: std::clone::Clone> ActionObserver<'a, T> {
     /// Possible usecases are for debug logging when you need to see variable value every time
     /// before it is changed or changing the value if needed
     /// ```rust
+    /// use observer::action::ActionObserver;
     /// static mut logging_fn: fn(&mut i32)->() = |val: &mut i32|{
     ///     println!("Current value is:{}",val);
     /// };
@@ -17,7 +18,7 @@ impl<'a, T: std::clone::Clone> ActionObserver<'a, T> {
     /// *val = 5;
     /// // you should see 'Current value is:1' in your terminal
     /// ```
-    fn new(value: T, handle_function: &'static mut dyn FnMut(&mut T) -> ()) -> Self {
+    pub fn new(value: T, handle_function: &'static mut dyn FnMut(&mut T) -> ()) -> Self {
         ActionObserver {
             value: value.clone(),
             func: handle_function,
@@ -32,7 +33,7 @@ impl<'a, T: std::clone::Clone> Deref for ActionObserver<'a, T> {
 }
 impl<'a, T: std::clone::Clone + std::cmp::PartialEq> DerefMut for ActionObserver<'a, T> {
     fn deref_mut(&mut self) -> &mut Self::Target {
-            (self.func)(&mut self.value);
+        (self.func)(&mut self.value);
         &mut self.value
     }
 }
